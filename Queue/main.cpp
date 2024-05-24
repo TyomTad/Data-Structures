25dc58fc2458a3cde0705d16d56767ae05aa7f55
TyomTad <138680836+TyomTad@users.noreply.github.com>
2024-05-24 21:07:28 +0400

    Add files via upload

---
+++ Queue/main.cpp
@@ -0,0 +1,79 @@
+#include "queue.h"
+#include <iostream>
+int main(int argc, char const *argv[])
+{
+   g3::queue<int> a {};
+
+   a.push(55);
+   // std::cout << a.top() << std::endl; 
+  //  while(!a.empty()) {
+  //  std::cout << a.top() <<  " ";
+   //   a.pop();
+   // }
+
+   g3::queue<int> s1 = {1,2,3,4,5,6,7,8,9};
+   s1.push(33);
+
+   if(a == s1){
+     std::cout << "hello" << std::endl;
+   }else {
+     std::cout << "not hello " << std::endl;
+   }
+  //  s1 = a; // copy assigment operator 
+  //  while(!s1.empty()) {
+  //     std::cout << s1.top() <<  " "; 
+  //     s1.pop();
+  //  }
+
+  //  s1 = std::move(a); // move assigment operator 
+  //  while(!s1.empty()) {
+  //      std::cout << s1.top() << " ";
+  //      s1.pop();
+  //  }
+
+  // a.push(55);
+  // a.push(33);
+  // a.push(22);
+  // while(!a.empty()) {
+  //   std::cout << a.top() << " ";
+  //   a.pop();
+  // }
+
+  //  if (a == s1 ) {
+  //     std::cout << "This queue is equal " << std::boolalpha << std::endl; 
+  //  } else {
+  //     std::cout << "This queue is not equal " << std::boolalpha << std::endl; 
+  //  }
+      
+      // if (a > s1) {
+      //    std::cout << "s1 grether then a " << std::boolalpha << std::endl; 
+      // } else {
+      //     std::cout << "s1 less then a " << std::boolalpha << std::endl; 
+      // }
+
+      // if (a < s1) {
+      //    std::cout << "s1 grether then a " << std::boolalpha << std::endl; 
+      // } else {
+      //     std::cout << "s1 less then a " << std::boolalpha << std::endl; 
+      // }
+
+      // if (a >= s1) {
+      //    std::cout << "s1 grether then a " << std::boolalpha << std::endl; 
+      // } else {
+      //     std::cout << "s1 less then a " << std::boolalpha << std::endl; 
+      // }
+
+      // if (a <= s1) {
+      //    std::cout << "s1 grether then a " << std::boolalpha << std::endl; 
+      // } else {
+      //     std::cout << "s1 less then a " << std::boolalpha << std::endl; 
+      // }
+
+      // if (a != s1) {
+      //    std::cout << "s1 is not equal a " << std::boolalpha << std::endl; 
+      // } else {
+      //     std::cout << "s1 is equal a " << std::boolalpha << std::endl; 
+      // }
+
+
+}
